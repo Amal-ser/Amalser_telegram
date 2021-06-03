56a83d6b677b090c686013d204fb08afc9966a30
joker153 <77005994+joker153@users.noreply.github.com>
2021-06-03 22:21:32 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -167,7 +167,7 @@ async def auto_filter(bot, update):
             InlineKeyboardButton(f"🔰 Page 1/{len_result if len_result < max_pages else max_pages} 🔰", callback_data="ignore")
         ])
         
-        result[0].append([ InlineKeyboardButton(f"⚠️JOIN OUR SECOND CHANNEL⚠️", url="https://t.me/joinchat/o2TAYsK2nc42MjQ1") ])       
+        result[0].append([ InlineKeyboardButton(f"🔎HOW TO SEARCH MOVIES🔍", url="https://t.me/movieuploader1/79") ])       
         # if show_invite is True Append invite link buttons
         if show_invite:
             
