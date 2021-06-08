29a5eb1a432720f110114855756b31c886e3e670
joker153 <77005994+joker153@users.noreply.github.com>
2021-06-08 16:57:41 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -294,4 +294,4 @@ async def recacher(group_id, ReCacheInvite=True, ReCacheActive=False, bot=Bot, u
             
             ACTIVE_CHATS[str(group_id)] = achatId
     return 
-© 2021 GitHub, Inc.
+
