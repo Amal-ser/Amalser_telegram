34edac8eba9a1e5b0850b51d8bf723dbfffe0520
joker153 <77005994+joker153@users.noreply.github.com>
2021-06-10 12:22:45 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -126,7 +126,7 @@ async def auto_filter(bot, update):
                 file_link = f"https://t.me/{bot_.username}?start={unique_id}"
             
             results.append([
-            InlineKeyboardButton(file_names, url=file_link),
+            InlineKeyboardButton( " 📂 " + file_names, url=file_link),
             InlineKeyboardButton(f_size, url=file_link)
         ])
         
