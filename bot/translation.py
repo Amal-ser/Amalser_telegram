0b7c025f5437f30030832480accd06a820096ca3
Cyber Chekuthan <80899895+cyberchekuthan@users.noreply.github.com>
2021-08-30 17:10:21 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,28 +5,28 @@
 class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
-<b>ഞാനൊരു അമാനുഷികനായ ബോട്ടാണ് ഞാൻ @Movieuploaders2 ഗ്രൂപ്പിലേക്ക് സിനിമ എല്ലാം കൊടുക്കുന്നത് ഞാനാണ്😁</b>
+<b>ഞാനൊരു അമാനുഷികനായ ബോട്ടാണ് ഞാൻ @malayalam_rocker_s ഗ്രൂപ്പിലേക്ക് സിനിമ എല്ലാം കൊടുക്കുന്നത് ഞാനാണ്😁</b>
 
 
 <b>ഇവിടെ ഇരുന്ന്  Start കുത്തി കളിക്കാതെ സ്ഥലം  വിട്ടോ😂</b>
 
 
-🕵‍♂ ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ 👉 <b><i><a href="https://t.me/Rfxjoker">🅹🅾🅺🅴🆁</a></i></b>
+🕵‍♂ ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ 👉 <b><i><a href="https://t.me/amal_aj">Amalser</a></i></b>
 </i>"""    
     
     HELP_TEXT = """
 <b>നോക്കിയിരുന്നോ ഇപ്പൊ ഹെൽപ്പ് ചെയ്യും😁</b>
             
- @movieuploaders
+ @malayalam_rocker_s
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/Rfxjoker">Rfxjoker</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/amal_aj">amal_aj</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://t.me/@Movieuploaders2">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://t.me/@Malayalam_rocker_s">Click Me</a></i>
 """
