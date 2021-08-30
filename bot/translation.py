84d6c1605cf2596e0a34211497106e9fdfb69d23
Cyber Chekuthan <80899895+cyberchekuthan@users.noreply.github.com>
2021-08-30 17:28:45 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -28,5 +28,5 @@ class Translation(object):
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://t.me/@Malayalam_rocker_s">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://t.me/@malayalam_rocker_s">Click Me</a></i>
 """
