609eefc13354c35472e1827787e26dcd8e166ab0
Cyber Chekuthan <80899895+cyberchekuthan@users.noreply.github.com>
2021-09-03 00:28:49 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -141,7 +141,7 @@ async def help(bot, update):
     
     await bot.send_message(
         chat_id=update.chat.id,
-        text=Translation.HELP_TEXT,
+        text=Translation.START_TEXT,
         reply_markup=reply_markup,
         parse_mode="html",
         reply_to_message_id=update.message_id
