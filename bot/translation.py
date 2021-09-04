c6121758fe82baca33272e39c8db465ed881c649
Sunaif_adkar <80641581+sunaif-adkar2@users.noreply.github.com>
2021-09-04 23:12:48 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -7,22 +7,15 @@ class Translation(object):
     START_TEXT = """<b>Hey {}!!</b>
 <b>I am bot created for @malayalam_rocker_s You all Welcome first😁</b>
 
-
-<b>First you Subcribe my Channel Then give start onces more then you will get files</b>
-
-<b> Youtube link 👉 : https://youtu.be/RlSG4ZgPls0</b>
-
-
 🕵‍♂ ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ 👉 <b><i><a href="https://t.me/amal_aj">Amalser</a></i></b>
 </i>"""    
     
     HELP_TEXT = """
-<b>Subcribe youtube channel first😁</b>
+<b>I can't help you (｡◕‿◕｡)</b>
             
- @malayalam_rocker_s
 """
     
-    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
+    ABOUT_TEXT = """<b>➥ N̆̈Ŏ̈T̆̈H̆̈Ĭ̈N̆̈Ğ̈ Ă̈B̆̈Ĭ̈Ŭ̈T̆̈ M̆̈Ĕ̈ 🙂</code>
     
 <b>➥ Creator</b> : <b><i><a href="https://t.me/amal_aj">amal_aj</a></i></b>
 
@@ -30,6 +23,7 @@ class Translation(object):
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://t.me/@malayalam_rocker_s">Click Me</a></i>
-<b>➥ 🎀Re Edit</b> : <b><a href="https://t.me/sunaif_adkar">👤Sunaif_adkar</a></b>
+<b>➥ join Group </b> : <i><a href="https://t.me/malayalam_rocker_s">malayalam_rockers</a></i>
+
+<b>➥ Source Code</b> : <b><a href="https://t.me/sabotschannel/12">CLICK HERE ⛱</a></b>
 """
