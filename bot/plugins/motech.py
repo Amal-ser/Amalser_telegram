2fa16a49a13069b093ba5048f067df93766ab93a
Sunaif_adkar <80641581+sunaif-adkar2@users.noreply.github.com>
2021-09-04 09:29:57 +0530

    bug hunted 😆🔥

---
+++ bot/plugins/motech.py
@@ -0,0 +1,18 @@
+"""eunning Code moTechYT
+editing By Pr0fess0r 99
+gnu affero general public license v3.0
+permissions of this strongest copyleft license
+are conditioned on making available complete
+source code of licensed works and modifications,
+which include larger works using a licensed work,
+under the same license. Copyright and license
+notices must be preserved. Contributors provide
+an express grant of patent rights.
+when a modified version is used to provide a service
+over a network, the complete source code of the modified
+version must be made available.
+»https://github.com/AlbertEinsteinTG»https://t.me/sabotschannel/12your main Channel Added from Heroku
+update"""
+MT_BOT_UPDATES = "https://t.me/sabotschannel/12" # eg
+"""your Main Channel"""
+BOT_UPDATES = "https://t.me/sunaif_adkar" # eg
